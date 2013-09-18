9d62ada443b5a777233ef67b4311dce848335bbf
chuqq <chuqingq@qq.com>
2013-09-18 09:46:55 +0800

    1

--- ls_callmodel.h
+++ ls_callmodel.h
@@ -11,9 +11,12 @@ typedef struct ls_callmodel_s {
     int dest;       /* 目标值 */
     int duration;   /* 达到目标值后的呼叫时常 */
 
-    int current;
+    int current;    /* 当前的呼叫数 */
     uv_timer_t accelerate_timer;
     uv_timer_t duration_timer;
+
+    ls_worker_t* workers;
+    int worker_num;
 } ls_callmodel_t;
 
 int read_callmodel(ls_callmodel_t* callmodel);
