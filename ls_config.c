59df8370817b3b050c2fb2616437b8deb8bece01
ChuQQ <chuqingq@qq.com>
2013-10-12 23:17:51 +0800

    use plain c implementation for ls_config_t

--- ls_config.c
+++ ls_config.c
@@ -1,4 +1,7 @@
 #include <cassert>
+#include <stdlib.h>
+#include <string.h>
+
 #include <fstream>
 using namespace std;
 
@@ -24,14 +27,31 @@ int load_config(ls_config_t* config) {
     config->worker_num = root["worker_num"].asInt();
     printf("  worker_num = %d\n", config->worker_num);
 
-    // plugin_paths
+    // allocate plugin_paths
+    config->plugin_paths = (char**) malloc((config->worker_num + 1) * sizeof(char*));
+    if (config->plugin_paths == NULL)
+    {
+        printf("ERROR failed to malloc plugin_paths.\n");
+        return -1;
+    }
+
+    // plugins_num and plugin_paths
     Json::Value plugin_paths = root["plugin_paths"];
-    for (size_t i = 0; i < plugin_paths.size(); ++i)
+    config->plugins_num = plugin_paths.size();
+    size_t i;
+    for (i = 0; i < config->plugins_num; ++i)
     {
         printf("  plugin_paths[%d] = %s\n", i, plugin_paths[i].asString().c_str());
-        config->plugin_paths.push_back(plugin_paths[i].asString());
+        // config->plugin_paths.push_back(plugin_paths[i].asString());
+        string path = plugin_paths[i].asString();
+        int len = path.length();
+
+        char* path_dst = (char*) malloc(len + 1);
+        memcpy(path_dst, path.c_str(), len + 1);
+        config->plugin_paths[i] = path_dst;
     }
-    
+    config->plugin_paths[i] = NULL;
+
     ifs.close();
     return 0;
 }
@@ -39,5 +59,14 @@ int load_config(ls_config_t* config) {
 int unload_config(ls_config_t* config) {
     printf("==== unload_config()\n");
     
+    for (size_t i = 0; i < config->plugins_num; ++i)
+    {
+        free(config->plugin_paths[i]);
+        config->plugin_paths[i] = NULL;
+    }
+    free(config->plugin_paths);
+    config->plugin_paths = NULL;
+    config->plugins_num = 0;
+
     return 0;
 }
