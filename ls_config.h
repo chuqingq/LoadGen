a2345c1101d7834967b606c56d4e8dbc89a31718
ChuQQ <chuqingq@qq.com>
2013-10-12 23:19:44 +0800

    use plain c implementation for ls_config_t

--- ls_config.h
+++ ls_config.h
@@ -1,13 +1,15 @@
 #ifndef LS_CONFIG_H_
 #define LS_CONFIG_H_
 
-#include <string>
-#include <vector>
-using namespace std;
+// #include <string>
+// #include <vector>
+// using namespace std;
 
 typedef struct {
     int worker_num;
-    vector<string> plugin_paths;
+    // vector<string> plugin_paths;
+    size_t plugins_num;
+    char** plugin_paths;
 } ls_config_t;
 
 int load_config(ls_config_t* config);
