ccd04f7d57e4f93a9759a12fe338fee457a50db2
chuqq <chuqingq@qq.com>
2013-10-29 12:59:38 +0800

    cap header macro

--- ls_utils.h
+++ ls_utils.h
@@ -1,5 +1,5 @@
-#ifndef LS_utils_H_
-#define LS_utils_H_
+#ifndef LS_UTILS_H_
+#define LS_UTILS_H_
 
 #include <stddef.h>
 #define container_of(ptr, type, member) ((type *)((char *) (ptr) - offsetof(type, member)))
